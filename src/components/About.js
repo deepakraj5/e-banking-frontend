f73e45dd1cacf403541ae9179755530564f61811
deepakraj5 <deepakatr4@gmail.com>
2020-11-02 10:28:31 +0530

    about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,48 @@
-import React from 'react'
+import React from "react";
 
-class About extends React.Component{
-    render(){
-        return(
-            <div className="container jumbotron shadow bg-white rounded">
-               <h2>About</h2>
-            </div>
-        );
-    }
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="container jumbotron shadow bg-white rounded">
+          <h2>About</h2>
+        </div>
+
+        <div className="container jumbotron shadow bg-white rounded">
+          <p>
+            Online banking, also known as internet banking or web banking, is an
+            electronic payment system that enables customers of a bank or other
+            financial institution to conduct a range of financial transactions
+            through the financial institution's website. The online banking
+            system will typically connect to or be part of the core banking
+            system operated by a bank and is in contrast to branch banking which
+            was the traditional way customers accessed banking services.
+          </p>
+
+          <p>
+            Some banks operate as a "direct bank" (or “virtual bank”), where
+            they rely completely on internet banking. Internet banking software
+            provides personal and corporate banking services offering features
+            such as viewing account balances, obtaining statements, checking
+            recent transactions, transferring money between accounts, and making
+            payments.
+          </p>
+
+          <p>
+            The first known deployment of home computer banking to consumers
+            came in December 1980 at United American Bank, a community bank
+            headquartered in Knoxville, Tenn. United American partnered with
+            Radio Shack to produce a secure custom modem for its TRS-80 computer
+            that would allow bank customers to access account information
+            securely. Services available in its first year included bill pay,
+            account balance checks, and loan applications, as well as game
+            access, budget and tax calculators and daily newspapers. Thousands
+            of customers paid $25–30 per month for the service.
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default About
+export default About;
--- src/components/Header.js
+++ src/components/Header.js
@@ -13,15 +13,15 @@ class Header extends React.Component{
                         <Nav className="ml-auto">
                             <NavLink to="/" className="nav-link h6" activeClassName="text-primary" exact={true}>Home</NavLink>
                             <NavLink to="/about" className="nav-link h6" activeClassName="text-primary">About</NavLink>
-                            <NavLink to="/careers" className="nav-link h6" activeClassName="text-primary">Careers</NavLink>
-                            <NavLink to="/contact" className="nav-link h6" activeClassName="text-primary">Cotact Us</NavLink>
-                            <NavLink to="/loan" className="nav-link h6" activeClassName="text-primary">Loan</NavLink>
+                            {/* <NavLink to="/careers" className="nav-link h6" activeClassName="text-primary">Careers</NavLink> */}
+                            {/* <NavLink to="/contact" className="nav-link h6" activeClassName="text-primary">Cotact Us</NavLink> */}
+                            {/* <NavLink to="/loan" className="nav-link h6" activeClassName="text-primary">Loan</NavLink> */}
                             <NavLink to="/products" className="nav-link h6" activeClassName="text-primary">Products</NavLink>
-                            <NavDropdown className="h6" title="Services" id="basic-nav-dropdown">
+                            {/* <NavDropdown className="h6" title="Services" id="basic-nav-dropdown">
                                 <NavLink to="/service1" className="dropdown-item" activeClassName="bg-info" exact={true}>Service 1</NavLink>
                                 <NavLink to="/service2" className="dropdown-item " activeClassName="bg-info">Service 2</NavLink>
                                 <NavLink to="/profile" className="dropdown-item " activeClassName="bg-info">Profile</NavLink>
-                            </NavDropdown>
+                            </NavDropdown> */}
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
